013086468d0e60b373914a5fb9cb252da2c47186
prapra1 <rohan.sethi@nagarro.com>
2018-02-19 19:12:02 +0530

    in

--- spring-boot-swagger-example-master/src/main/java/com/techprimers/springboot/swaggerexample/resource/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.techprimers.springboot.swaggerexample.resource;
-
-public class Constants {
-	public static final String username = "root";
-	public static final String password = "nagarro";
-	public static final String url = "jdbc:mysql://localhost:3306/tube?useUnicode=true&amp;characterEncoding=UTF-8";
-
-}
